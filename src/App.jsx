377c9469e880422a54a8d4043741ff357f2236d7
21colussi <21colussi@gmail.com>
2025-12-16 23:31:45 +0900

    Create Hero

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import Header from './components/header/Header';
 import './App.css'
 import Menu from './components/menu/Menu';
+import Hero from './components/hero/Hero';
 
 function App() {
   const [menuOpen, setMenuOpen] = useState(false);
   const toggleMenu = () => setMenuOpen(prev => !prev);
   return (
     <>
+      <Hero/>
       <Header open={menuOpen} toggle={toggleMenu}/>
       <main className={menuOpen ? "open" : "closed"}>
         <div className="content">
