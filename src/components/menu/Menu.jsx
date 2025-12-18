d7fb590538eeb108a03257dc7498bf1273c28e5b
21colussi <21colussi@gmail.com>
2025-12-18 19:09:39 +0900

    Edit Header/Menu

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,29 +1,31 @@
 import '../../styles/menu/menu.css';
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function Menu({ open }) {
+    const [isDark, setIsDark] = useState(false);
     useEffect(() => {
         const savedTheme = localStorage.getItem("theme");
         if (savedTheme === "dark") {
             document.documentElement.setAttribute("data-theme", "dark");
+            setIsDark(true);
         }
     }, []);
     const toggleTheme = () => {
         const root = document.documentElement;
-        const currentTheme = root.getAttribute("data-theme");
-        if (currentTheme === "dark") {
-            root.removeAttribute("data-theme");
+        if (isDark) {
+            root.removeAttribute("data-theme"); // light
             localStorage.setItem("theme", "light");
+            setIsDark(false);
         } else {
             root.setAttribute("data-theme", "dark");
             localStorage.setItem("theme", "dark");
+            setIsDark(true);
         }
     };
     return (
         <aside id="menu" className={`menu ${open ? 'open' : 'closed'}`}>
             <div className="menu-top" aria-hidden={!open}>
                 <h3 className="menu-logo">
-                    {/*<img className='logo-icon' alt='' src='temp-logo.svg'/>*/}
                     <span>Théo</span>
                 </h3>
                 <ul>
@@ -37,29 +39,32 @@ function Menu({ open }) {
                     ))}
                 </ul>
             </div>
-            <div className='menu-bottom'>
+            <div className="menu-bottom">
                 <div className="socials" aria-hidden={!open}>
                     <h5>Réseaux sociaux</h5>
                     <ul>
                         <li>
-                            <i class="fa-brands fa-tiktok"/>
+                            <i className="fa-brands fa-tiktok" />
                             <span>théo</span>
                         </li>
                         <li>
-                            <i class="fa-brands fa-instagram"></i>
+                            <i className="fa-brands fa-instagram" />
                             <span>theoclss2</span>
                         </li>
                     </ul>
                 </div>
                 <div className="theme-color" aria-hidden={!open}>
                     <h5>Thème</h5>
-                    <button onClick={toggleTheme} tabIndex={open ? 0 : -1}>
-                        <span>O</span>
+                    <button
+                        onClick={toggleTheme}
+                        tabIndex={open ? 0 : -1}
+                    >
+                        <i className={`fa-solid ${isDark ? "fa-sun" : "fa-moon"}`} />
                     </button>
                 </div>
             </div>
         </aside>
-    )
+    );
 }
 
 export default Menu;
